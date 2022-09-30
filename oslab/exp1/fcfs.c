feea13833d4ba2a5f3ab6dc541b041999a94da23
204g1a0565 <112164158+204g1a0565@users.noreply.github.com>
2022-09-30 15:18:21 +0530

    Update fcfs.c

--- oslab/exp1/fcfs.c
+++ oslab/exp1/fcfs.c
@@ -1,7 +1,3 @@
-/*Name of the Candidate :L.Narayana Reddy
-Roll no : 204g10576
-Title of the Experiment : CPU Scheduling using FCFS Algorithm
-Date of Creation : 16-09-2022 */
 #include<stdio.h>
 int main()
 {
